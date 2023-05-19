9a9d8d73b38d1f0212201e804d2bfa3027dc6e55
semihcaliskan13 <1030510071@erciyes.edu.tr>
2023-05-19 15:19:53 +0300

    sqltabledependecy and signalR

---
+++ Infrastructure/StockTracking.Infrastructure/SqlTableDependency/Middleware/DatabaseSubscriptionMiddleware.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.AspNetCore.Builder;
+
+namespace StockTracking.Infrastructure.SqlTableDependency.Middleware
+{
+    static public class DatabaseSubscriptionMiddleware
+    {
+        public static void UseDatabaseSubscription<T>(this IApplicationBuilder builder, string tableName) where T : class , IDatabaseSubscription
+        {
+            var sub = (T)builder.ApplicationServices.GetService(typeof(T));
+            sub.Configure(tableName);
+        }
+    }
+}
